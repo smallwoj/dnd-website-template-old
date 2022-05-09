40537f87c319ad388864607429700625241a8086
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2022-05-08 23:45:21 -0500

    better recaps

--- src/assets/recap.md
+++
@@ -1,9 +0,0 @@
-
-* 1. [Session 1: May 8th, 2022](#Session1:May8th2022)
-
-
-##  1. <a name='Session1:May8th2022'></a>Session 1: May 8th, 2022
-oh yeah a bunch of stuff happened
-
-some guy decided to do something.
-isnt that neat?
--- src/views/RecapsView.vue
+++ src/views/RecapsView.vue
@@ -1,16 +1,35 @@
 <template>
   <v-container>
-    <h1>Okay come get your freaking recap</h1>
-    <Editor
-      :value="markdown"
-      mode="viewer"
-    />
+    <v-expansion-panels
+      v-model="opened"
+      multiple
+    >
+      <v-expansion-panel
+        v-for="(recap, index) in recapEntries"
+        :key="index"
+      >
+        <v-expansion-panel-header>
+          <h3>Session {{ index + 1 }} - {{ recap.dateString }}</h3>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <Editor
+            :value="recap.content"
+            mode="viewer"
+          />
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </v-container>
 </template>
 
 <script>
 import { Editor } from 'vuetify-markdown-editor';
-import recapMarkdown from 'raw-loader!@/assets/recap.md'
+
+const allFiles = (ctx => {
+  let keys = ctx.keys();
+  let values = keys.map(ctx);
+  return keys.reduce((o, k, i) => { o[k] = values[i]; return o; }, {});
+})(require.context('raw-loader!@/assets/recaps', true, /.*/));
 
 export default {
   name: 'RecapsView',
@@ -19,17 +38,50 @@ export default {
     Editor,
   },
 
+  data() {
+    return {
+      opened: [Object.keys(allFiles).length - 1],
+    };
+  },
+
   computed: {
-    markdown() {
-      return recapMarkdown;
-    }
+    markdownFiles() {
+      return allFiles;
+    },
+
+    recapEntries() {
+      const entries = Object.keys(this.markdownFiles).map((fileName) => {
+        const noExtension = this.stripExtension(fileName);
+        const date = new Date(noExtension);
+        const dateString = date.toLocaleDateString('en-US', {
+          month: 'long',
+          day: 'numeric',
+          year: 'numeric',
+          timeZone: 'UTC',
+        });
+
+        return {
+          date,
+          dateString,
+          content: this.markdownFiles[fileName].default,
+        };
+      });
+      return entries.sort((a, b) => a.date - b.date);
+    },
+  },
+
+  methods: {
+    stripExtension(file) {
+      const fileName = file.replace(/^\.\//, '');
+      return fileName.replace(/\.[^/.]+$/, '');
+    },
   },
 }
 </script>
 
 <style lang="scss">
 p {
-  padding-top: 2.5px;
-  padding-bottom: 2.5px;
+  padding-top: 5px;
+  padding-bottom: 5px;
 }
 </style>
