3e8c4ac18eeae14885b3bf4bd61f17d0df937724
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2022-05-08 21:36:21 -0500

    starting recaps

--- src/views/RecapsView.vue
+++ src/views/RecapsView.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1>RecapsView</h1>
+  <v-container>
+    <h1>Okay come get your freaking recap</h1>
+    <Editor
+      :value="markdown"
+      mode="viewer"
+    />
+  </v-container>
 </template>
 
 <script>
-export default {
+import { Editor } from 'vuetify-markdown-editor';
+import recapMarkdown from 'raw-loader!@/assets/recap.md'
 
+export default {
+  name: 'RecapsView',
+
+  components: {
+    Editor,
+  },
+
+  computed: {
+    markdown() {
+      return recapMarkdown;
+    }
+  },
 }
 </script>
+
+<style lang="scss">
+p {
+  padding-top: 2.5px;
+  padding-bottom: 2.5px;
+}
+</style>
