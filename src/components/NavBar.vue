75e6bf589e3fe8b4ce32cc191c6bbb0dad077c8c
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2022-05-08 20:41:49 -0500

    Set up navigation

---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-tabs>
+      <v-tab
+        v-for="route in routes"
+        :key="route.path"
+        :to="route.path"
+      >
+        {{ route.name }}
+      </v-tab>
+    </v-tabs>
+  </v-container>
+</template>
+
+<script>
+import routes from '@/router/routes';
+
+export default {
+  name: 'NavBar',
+  data() {
+    return {
+      routes: routes,
+    };
+  },
+};
+</script>
---
+++ src/views/RecapsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>RecapsView</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
