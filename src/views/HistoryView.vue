be0f096ad29b9202f8dbda00bd7a3d94e0796879
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2022-06-20 00:22:00 -0500

    create history view

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,9 +1,41 @@
 <template>
-  <h1>HistoryView</h1>
+  <v-timeline>
+    <v-timeline-item
+      v-for="item in history"
+      :key="item.header"
+      :color="item.color"
+      :icon="item.icon"
+      fill-dot
+    >
+      <v-card>
+        <v-card-title
+          class="text-h5"
+          :style="`color: ${item.color}`"
+        >
+          {{ item.header }}
+        </v-card-title>
+        <v-card-text>
+          {{ item.description }}
+        </v-card-text>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
 </template>
 
 <script>
-export default {
+import history from '../assets/history.yaml';
 
+export default {
+  data() {
+    return {
+      history,
+    };
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.v-timeline::before {
+  position: relative;
+}
+</style>
