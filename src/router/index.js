59a256f0a4b5123d4d4e45ec1116e30f4ca8a172
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2022-06-26 16:14:25 -0500

    Window title

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
+import globals from '../globals';
 
 Vue.use(VueRouter);
 
@@ -8,6 +9,12 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const title = document.querySelector('title');
+  title.innerHTML = `${to.name} - ${globals.app}`;
+  next();
 });
 
 export default router;
